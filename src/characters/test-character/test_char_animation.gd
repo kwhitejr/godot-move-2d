0a68ea76391035ac9f60bb5d1bcd8e5bd3ac9229
Kevin White <kwhitejr@gmail.com>
2023-11-06 12:33:50 -0800

    All Godot files to src

---
+++ src/characters/test-character/test_char_animation.gd
@@ -0,0 +1,66 @@
+extends AnimatedSprite2D
+
+
+const do_not_interrupt = ["dash"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_char_idle_stand(body : CharacterBody2D):
+#	if do_not_interrupt.has(self.get_animation()):
+#		return
+
+	if is_zero_approx(body.velocity.x) and body.is_on_floor():
+		self.play("idle_stand")
+
+func _on_char_idle_crouch():
+	self.play("idle_crouch")
+
+func _on_jump_component_char_jump_ascend():
+	self.play("jump_ascend")
+
+func _on_jump_component_char_jump_apex():
+	self.play("jump_apex")
+
+func _on_jump_component_char_jump_air():
+	self.play("jump_air")
+
+func _on_jump_component_char_jump_descend():
+	self.play("jump_descend")
+
+func _on_jump_component_char_jump_landed():
+	self.play("jump_land")
+	self.material.set_shader_parameter("enabled", 0)
+
+func _on_crouch_component_char_idle_crouch():
+	self.play("idle_crouch")
+
+func _on_dash_component_char_dash_start():
+	self.play("dash")
+
+func _on_dash_component_char_dash_end():
+	self.play("idle_stand")
+
+func _on_walk_component_char_walk(body: CharacterBody2D):
+	if do_not_interrupt.has(self.get_animation()):
+		return
+	if body.is_on_floor():
+		self.play("walk")
+
+func _on_test_char_char_face_direction(direction: float):
+	var should_flip_horizontal = direction == -1
+	self.flip_h = should_flip_horizontal
+
+func _on_jump_component_char_visualize_feature_enable(color: Color) -> void:
+	var is_enabled := 1
+	self.material.set_shader_parameter("enabled", is_enabled)
+	self.material.set_shader_parameter("color", color)
+
+func _on_jump_component_char_visualize_feature_disable():
+	var is_enabled := 0
+	self.material.set_shader_parameter("enabled", is_enabled)
