f37327fe76395e50bbaed02577546b51d7c40169
Kevin White <kwhitejr@gmail.com>
2023-11-22 15:07:44 -0800

    Update documentation for Jump

--- src/godot-move-2d/abstract_move_component.gd
+++ src/godot-move-2d/abstract_move_component.gd
@@ -11,23 +11,23 @@ func _ready() -> void:
 
 ## Called in _ready()
 ## Can be implemented without overriding base class _ready() functionality
-func handle_ready(body: CharacterBody2D) -> void:
+func handle_ready(_body: CharacterBody2D) -> void:
 	pass
 
 ## Returns boolean indicating whether the action can be performed.
 ## Override for custom move conditions.
 ## @example: res://demos/celeste-clone/jump_component_override.gd
-func can_do_move(body: CharacterBody2D) -> bool:
+func can_do_move(_body: CharacterBody2D) -> bool:
 	return true
 
 # Called in CharacterBody2D._input()
-func handle_input_event(event : InputEvent, body: CharacterBody2D) -> void:
+func handle_input_event(_event : InputEvent, _body: CharacterBody2D) -> void:
 	pass
 
 # Called in CharacterBody2D._process()
-func handle_process(delta: float, body: CharacterBody2D) -> void:
+func handle_process(_delta: float, _body: CharacterBody2D) -> void:
 	pass
 
 # Called in CharacterBody2D._physics_process()
-func handle_physics_process(delta: float, body: CharacterBody2D, previous_velocity: Vector2) -> void:
+func handle_physics_process(_delta: float, _body: CharacterBody2D, _previous_velocity: Vector2) -> void:
 	pass
--- src/godot-move-2d/jump_component.gd
+++ src/godot-move-2d/jump_component.gd
@@ -2,24 +2,35 @@ class_name JumpComponent
 extends AbstractMoveComponent
 
 
+## Configuration settings for the Jump movement.
+@export_group("Jump Configuration")
+## The height of the jump.
 @export var jump_height : float = 30
-@export var jump_time_to_peak : float = 0.5
+## The amount of time in seconds to reach the apex of the jump.
+@export var jump_time_to_apex : float = 0.5
+## The amount of time in seconds to descend the distance of the jump_height.
 @export var jump_time_to_descent : float = 0.25
-
+## The amount of air jumps allowed. Zero means air jumps are disabled. 
 @export var air_jumps_total : int = 2
-
+## Whether "coyote time" is enabled. 
+## "Coyote time" is the period during which, after leaving the ground, a character may still jump.
 @export var is_coyote_time_enabled : bool = true
+## The duration of coyote time in seconds.
 @export var coyote_time_duration_secs : float = 0.1
-@export var is_visualize_coyote_time_enabled : bool = false
-@export var coyote_time_feature_color : Color = Color(Constants.FeatureVisualizationColors.BLUE)
-
+## Whether "jump buffer" is enabled. 
+## "Jump buffer" is the period during which, before hitting the ground, a player's jump input will register as if player was on the ground.
 @export var is_jump_buffer_enabled : bool = true
 @export var jump_buffer_duration_secs : float = 0.2
+
+## Configuration settings for visualizing the Jump movement.
+@export_group("Jump Feature Visualization")
+@export var is_visualize_coyote_time_enabled : bool = false
+@export var coyote_time_feature_color : Color = Color(Constants.FeatureVisualizationColors.BLUE)
 @export var is_visualize_jump_buffer_enabled : bool = false
 @export var jump_buffer_feature_color : Color = Color(Constants.FeatureVisualizationColors.PINK)
 
-var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
-var jump_ascend_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_apex) * -1.0
+var jump_ascend_gravity : float = ((-2.0 * jump_height) / (jump_time_to_apex * jump_time_to_apex)) * -1.0
 var jump_descend_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 var air_jumps_current : int = air_jumps_total
 var is_jumping : bool = false
@@ -27,10 +38,15 @@ var is_jumping : bool = false
 var coyote_timer := Timer.new()
 var jump_buffer_timer := Timer.new()
 
+## Fired when the character jump ascension begins.
 signal char_jump_ascend
+## Fired when the character reaches the jump apex.
 signal char_jump_apex
+## Fired when the character jump descension begins.
 signal char_jump_descend
+## Fired when the character lands on ground during a jump.
 signal char_jump_landed
+## Fired when the character performs an air jump.
 signal char_jump_air
 
 func handle_ready(body: CharacterBody2D):
@@ -67,23 +83,28 @@ func handle_physics_process(delta: float, body: CharacterBody2D, previous_veloci
 	if _should_jump_land(body, is_jumping, previous_velocity):
 		_jump_land()
 
+## Returns a float representing the gravity during a jump.
 func get_jump_gravity(body : CharacterBody2D) -> float:
 	return jump_ascend_gravity if body.velocity.y < 0.0 else jump_descend_gravity
 
+## Returns a boolean representing whether the character can jump.
 func can_do_move(body: CharacterBody2D) -> bool:
 	if is_coyote_time_enabled and not body.is_on_floor():
 		return coyote_timer.get_time_left() > 0
 	return body.is_on_floor()
 
+## Encapsulates logic for a Jump movement.
 func _jump(body: CharacterBody2D) -> void:
 	air_jumps_current = air_jumps_total
 	body.velocity.y = jump_velocity
 	is_jumping = true
 	char_jump_ascend.emit()
 
+## Returns a boolean representing whether the character can air jump.
 func _can_jump_air(body: CharacterBody2D, is_jumping: bool, remaining_air_jumps: int) -> bool:
 	return remaining_air_jumps > 0 and is_jumping and not body.is_on_floor()
-	
+
+## Encapsulates logic for an air jump movement.
 func _jump_air(body: CharacterBody2D) -> void:
 	air_jumps_current -= 1
 	body.velocity.y = jump_velocity
@@ -119,6 +140,7 @@ func _start_jump_buffer_timer() -> void:
 	if is_visualize_jump_buffer_enabled:
 		char_visualize_feature_enable.emit(jump_buffer_feature_color)
 
+## Encapsulates logic for jump buffer.
 func _handle_jump_buffer(body: CharacterBody2D) -> void:
 	if jump_buffer_timer.get_time_left() > 0 and can_do_move(body):
 		jump_buffer_timer.stop()
